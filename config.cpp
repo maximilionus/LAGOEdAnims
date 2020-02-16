c4ba66b9ba2a72f0a524c02452a5b504edc1bfa4
maximilionus <xxxarakxxx@gmail.com>
2020-02-16 15:51:51 +0300

    Initial commit

---
+++ config.cpp
@@ -0,0 +1,18 @@
+#define ANIMATION_INTERPOLATION_SPEED 6 // Speed of interpolationSpeed can be set here
+
+class CfgPatches
+{
+	class LAGOEdAnims
+	{
+		// Meta information for editor
+		name = "LAGO Edition Animations";
+		author = "MAXIMILI";
+		url = "";
+		requiredVersion = 1.60; 
+		requiredAddons[] = {"A3_Anims_F_Config_Sdr"};
+		units[] = {};
+		weapons[] = {};
+	};
+};
+
+#include "animations.hpp"
